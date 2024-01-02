74f473a955526dd7522d6d0b5f520e5d590f0b07
Dhruv Shakti Singh Rawat <109079210+DhruvShaktiSinghRawat@users.noreply.github.com>
2024-01-02 16:06:15 +0530

    Add files via upload

---
+++ response.js
@@ -0,0 +1,8 @@
+const responseObj = {
+    hello: "Hey! how are you doing ? ",
+    hi: "Hello! ",
+    date:new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    location: "You are in Palwal.", 
+    f1infotechcompany: "https://www.f1infotech.com/",
+}
